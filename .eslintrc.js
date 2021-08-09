f8509ab11c870ae8058d1242275244d90f36657b
@manzuni <20832842+Madalin-Antonoiu@users.noreply.github.com>
2021-08-09 13:32:07 +0100

    Added babel-eslint for arrow f declaration inside classes and state = {}

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard", "prettier"],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {},
 };
